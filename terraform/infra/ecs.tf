89100b240b2149f3565f132ae8ecf5d8a9e583b7
adamkoy <adam.koyuncu@vechain.org>
2025-09-12 10:26:49 -0300

    working ecs deployment

---
+++ terraform/infra/ecs.tf
@@ -0,0 +1,55 @@
+
+# ---------- Logging ----------
+resource "aws_cloudwatch_log_group" "svc" {
+  name              = "/ecs/${var.service_name}"
+  retention_in_days = 7
+}
+
+# ---------- ECS ----------
+resource "aws_ecs_cluster" "this" {
+  name = "${var.service_name}-cluster"
+}
+
+# Task execution role (pull image from ECR, write logs)
+data "aws_iam_policy_document" "ecs_tasks_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "task_exec" {
+  name               = "${var.service_name}-exec-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_tasks_assume.json
+}
+
+resource "aws_iam_role_policy_attachment" "exec_logs" {
+  role       = aws_iam_role.task_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# ---------- Service ----------
+resource "aws_ecs_service" "svc" {
+  name            = "${var.service_name}-svc"
+  cluster         = aws_ecs_cluster.this.id
+  task_definition = aws_ecs_task_definition.svc.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = local.ecs_subnets
+    security_groups  = [aws_security_group.tasks.id]
+    assign_public_ip = local.assign_public_ip
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.this.arn
+    container_name   = "prism"
+    container_port   = var.container_port
+  }
+
+  depends_on = [aws_lb_listener.http]
+}
---
+++ terraform/infra/security-groups.tf
@@ -0,0 +1,42 @@
+# ---------- Security Groups ----------
+resource "aws_security_group" "alb" {
+  name   = "${var.service_name}-alb-sg"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    description = "HTTP from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "All egress"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "tasks" {
+  name   = "${var.service_name}-tasks-sg"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+    description     = "Only ALB can reach tasks"
+    from_port       = var.container_port
+    to_port         = var.container_port
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  egress {
+    description = "All egress"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
