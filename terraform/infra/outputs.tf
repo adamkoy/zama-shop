e498d0e7b0d265d8995e66b1e0ec663f629e6715
Adam Koyuncu <50804772+adamkoy@users.noreply.github.com>
2025-09-15 19:03:02 -0300

    working apigw with api key in terraform (#6)

--- terraform/infra/outputs.tf
+++ terraform/infra/outputs.tf
@@ -9,3 +9,17 @@ output "public_subnets" {
 output "private_subnets" {
   value = module.vpc.private_subnets
 }
+output "alb_dns_name" {
+  value       = aws_lb.this.dns_name
+  description = "ALB DNS name"
+}
+
+output "alb_url" {
+  value       = "http://${aws_lb.this.dns_name}"
+  description = "Convenience HTTP URL for curl"
+}
+
+output "service_name" {
+  value       = var.service_name
+  description = "Service name used for log groups"
+}
