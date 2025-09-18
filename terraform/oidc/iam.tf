da197e524915acb228770e907b6ef3246b6d3657
adamkoy <adam.koyuncu@vechain.org>
2025-09-18 10:11:13 -0300

    updated oidc github to create resources

--- terraform/oidc/iam.tf
+++ terraform/oidc/iam.tf
@@ -1,8 +1,6 @@
 
 locals {
   aws_region = "eu-west-3"
-  repo_name  = "zama-shop"  
-  repo_arn   = "arn:aws:ecr:${local.aws_region}:${data.aws_caller_identity.current.account_id}:repository/${local.repo_name}"
 }
 
 resource "aws_iam_role" "github_actions" {
@@ -10,53 +8,10 @@ resource "aws_iam_role" "github_actions" {
   assume_role_policy = data.aws_iam_policy_document.github_trust.json
 }
 
-# ECR permissions for GitHub Actions
-data "aws_iam_policy_document" "ecr_repo_access" {
-  # Login to ECR
-  statement {
-    sid       = "ECRAuthToken"
-    effect    = "Allow"
-    actions   = ["ecr:GetAuthorizationToken"]
-    resources = ["*"]
-  }
 
-  # Allow creating the repo if it doesn't exist
-  statement {
-    sid       = "CreateRepositoryIfMissing"
-    effect    = "Allow"
-    actions   = ["ecr:CreateRepository"]
-    resources = ["*"]
-  }
-
-  # Push/Pull/Describe on your specific repo
-  statement {
-    sid    = "RepoPushPull"
-    effect = "Allow"
-    actions = [
-      "ecr:BatchCheckLayerAvailability",
-      "ecr:CompleteLayerUpload",
-      "ecr:GetDownloadUrlForLayer",
-      "ecr:BatchGetImage",
-      "ecr:InitiateLayerUpload",
-      "ecr:PutImage",
-      "ecr:UploadLayerPart",
-      "ecr:ListImages",
-      "ecr:BatchDeleteImage",
-      "ecr:DescribeImageScanFindings",
-      "ecr:Describe*",
-    ]
-    resources = [local.repo_arn]
-  }
-}
-
-resource "aws_iam_policy" "ecr_repo_access" {
-  name   = "zama-shop-github-ecr"
-  policy = data.aws_iam_policy_document.ecr_repo_access.json
-}
-
-resource "aws_iam_role_policy_attachment" "attach_ecr" {
+resource "aws_iam_role_policy_attachment" "attach_admin" {
   role       = aws_iam_role.github_actions.name
-  policy_arn = aws_iam_policy.ecr_repo_access.arn
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
 output "github_actions_role_arn" {
